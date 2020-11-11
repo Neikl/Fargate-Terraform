8ea3e17120fafff9558d17feec0414c40014df3f
Neikl <70577741+Neikl@users.noreply.github.com>
2020-11-11 11:46:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there, Welcome to the sample node application - update.
+          Hi there, Welcome to the sample node application - v1.
         </p>
         <a
           className="App-link"
